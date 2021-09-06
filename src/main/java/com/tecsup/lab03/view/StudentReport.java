e356bfbbfbd874f8efa71b78fb24cb66f4f7a02e
Aimar Glover Oribe Vigo <aimaroribe@gmail.com>
2021-09-06 16:18:06 -0500

    studentreport

---
+++ src/main/java/com/tecsup/lab03/view/StudentReport.java
@@ -0,0 +1,10 @@
+package com.tecsup.lab03.view;
+
+public class StudentReport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola student report");
+	}
+
+}
