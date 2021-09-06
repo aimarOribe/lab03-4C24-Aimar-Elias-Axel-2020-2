fceabc69a0b03e5d4fee7ede2929c934db5172fd
Aimar Glover Oribe Vigo <aimaroribe@gmail.com>
2021-09-06 16:53:22 -0500

    final

--- src/main/java/com/tecsup/lab03/controller/StudentController.java
+++ src/main/java/com/tecsup/lab03/controller/StudentController.java
@@ -4,7 +4,7 @@ import com.tecsup.lab03.model.Student;
 
 public class StudentController {
     public static void main(String[] args) {
-        Student std = new Student("Generic", 2);
+        Student std = new Student("Generic", 56);
         std.print();
     }
 }
